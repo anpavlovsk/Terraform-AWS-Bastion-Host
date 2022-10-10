cd9ce42159ed90ed29f9fd202b1ae7cdc7ac4dcb
Andrii Pavlovskyi <anpavlovsk@gmail.com>
2022-10-10 15:54:53 +0200

    First commit

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+
+variable region {
+    default="eu-west-1"
+}
+
+#variable profile {
+#    default="personal"
+#Ã}
+
+variable instance-ami {
+    default="ami-0964eb2dc8b836eb6"
+}
+
+variable key_path {
+    default = "keys/mykeypair.pub"
+}
